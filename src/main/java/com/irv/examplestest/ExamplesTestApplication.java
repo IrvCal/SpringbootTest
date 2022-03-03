51997b82bd4112ff998b1143ebaf52b4cc7f08d4
irvcal <juanirvincalderon@gmail.com>
2022-03-03 12:03:43 -0600

    first commit

---
+++ src/main/java/com/irv/examplestest/ExamplesTestApplication.java
@@ -0,0 +1,13 @@
+package com.irv.examplestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irv/examplestest/ExamplesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irv.examplestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
